64bd47c610c2f8fe89c22457f9a57c7aac29c152
daryavasilyeva <darya.vasilyeva@phystech.edu>
2018-10-14 14:37:17 +0300

    Refactor

--- hangman/__init__.py
+++ hangman/__init__.py
@@ -1,18 +1,24 @@
+"""" Hangman-game"""
 import random
 
-dictionary = ['mother',
-              'father',
-              'permission',
-              'flight',
-              'apple',
-              'desire',
-              'bank',
-              'condition']
-i = random.randint(0, len(dictionary) - 1)
-initial_word = dictionary[i]
+
+def __init__():
+    """" Hangman initialisation"""
+    dictionary = ['mother',
+                  'father',
+                  'permission',
+                  'flight',
+                  'apple',
+                  'desire',
+                  'bank',
+                  'condition']
+    j = random.randint(0, len(dictionary) - 1)
+    initial_word = dictionary[j]
+    game(initial_word)
 
 
 def check_letter(check_word, letter):
+    """" check_letter"""
     for i in check_word:
         if letter == i:
             letter_check = 1
@@ -21,43 +27,43 @@ def check_letter(check_word, letter):
     return letter_check
 
 
-def reveal_letter(real_word, current_word, l):
-    for i in range(len(real_word)):
-        if real_word[i] == l:
-            current_word[i] = l
+def reveal_letter(real_word, current_word, letter):
+    """" reveal_letter-game"""
+    k = 0
+    for i in real_word:
+        if i == letter:
+            current_word[k] = letter
+        k += 1
     return current_word
 
 
 def print_result(error, real_word):
+    """" print_result"""
     if error == 5:
         print("You lost!")
         print("Real word is:", real_word)
-        return 0
     else:
         print("You won!")
-        return 1
 
 
 def game(word):
+    """game"""
     current_word = ['*' for i in range(len(word))]
     error = 0
     while error < 5 and '*' in current_word:
         print('Guess a letter:')
-        a = input()
+        input_letter = input()
         print(' ')
-        letter_check = check_letter(word, a)
+        letter_check = check_letter(word, input_letter)
         if letter_check == 1:
             print('Hit!')
-            current_word = reveal_letter(word, current_word, a)
+            current_word = reveal_letter(word, current_word, input_letter)
         if letter_check == 0:
             error += 1
             print("Missed, mistake", error, "out of 5")
         print("The word: ", end='')
-        for l in current_word:
-            print(l, end='')
+        for letter in current_word:
+            print(letter, end='')
         print(' ')
         print(' ')
     print_result(error, word)
-
-
-game(initial_word)
--- tests/tests.py
+++ tests/tests.py
@@ -1,14 +1,12 @@
-from hangman import check_letter, reveal_letter, print_result
+"""Tests"""
+from hangman import check_letter, reveal_letter
 
 
 def test_check_letter():
+    """check_letter"""
     assert check_letter("world", "a") == 0
 
 
 def test_reveal_letter():
-    assert reveal_letter("test", ['*', 'e', 's', '*'], "t") \
-           == ['t', 'e', 's', 't']
-
-
-def test_print_result():
-    assert print_result(4, "pool") == 1
+    """reveal_letter"""
+    assert reveal_letter("te", ['*', 'e'], "t") == ['t', 'e']
