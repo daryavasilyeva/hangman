5e40086cd7169c4fb910ac7340b43fc060da41c2
daryavasilyeva <darya.vasilyeva@phystech.edu>
2018-10-14 15:28:56 +0300

    Add tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,5 @@
 """Tests"""
-from hangman import check_letter, reveal_letter
+from hangman import check_letter, reveal_letter, init, print_result
 
 
 def test_check_letter():
@@ -10,3 +10,13 @@ def test_check_letter():
 def test_reveal_letter():
     """reveal_letter"""
     assert reveal_letter("te", ['*', 'e'], "t") == ['t', 'e']
+
+
+def test_init():
+    """test init"""
+    assert init() == 0
+
+
+def test_print_result():
+    """test print result"""
+    assert print_result(5, "word") == 0
